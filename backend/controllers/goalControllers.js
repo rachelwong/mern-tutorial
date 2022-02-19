1a0988e4ec8aa2cea53f68d697f6671e8c073d9a
rachel-wong <hello.rachelwong@gmail.com>
2022-02-19 19:14:03 +1000

    feat: added async handler to goal routes

--- backend/controllers/goalControllers.js
+++ backend/controllers/goalControllers.js
@@ -1,14 +1,18 @@
+const asyncHandler = require('express-async-handler');
+// when using mongoose to interact with database, we get back a promise. so async is needed
+// asyncHandler replaces try catch block
+
 // @desc GET Goals
 // @route GET /api/goals
 // @access Private
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
   res.status(200).json({message: 'GET goals'})
-}
+})
 
 // @desc SET Goals
 // @route POST /api/goals
 // @access Private
-const setGoal = (req, res) => {
+const setGoal = asyncHandler(async (req, res) => {
   console.log(req.body)
 
   if (!req.body.text) {
@@ -16,25 +20,25 @@ const setGoal = (req, res) => {
     throw new Error('Please add a text field to set goals')
   }
   res.status(200).json({message: 'SET goals'})
-}
+})
 
 // @desc UPDATE Goals
 // @route PUT /api/goals/:id
 // @access Private
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler( async (req, res) => {
   res
     .status(200)
     .json({ message: `Update goal ${req.params.id}` });
-}
+})
 
 // @desc DELETE Goal
 // @route Delete /api/goals/:id
 // @access Private
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
   res
     .status(200)
     .json({ message: `Delete goal ${req.params.id}` });
-}
+})
 
 module.exports = {
   getGoals,
