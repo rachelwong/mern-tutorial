107ecdd6de01e0d689ba9cdfba35bd6ff8807641
rachel-wong <hello.rachelwong@gmail.com>
2022-02-19 20:01:45 +1000

    feat: added crud functionality for goals

---
+++ backend/models/goalModel.js
@@ -0,0 +1,14 @@
+//define schema, fields for this resource
+
+const mongoose = require('mongoose')
+
+const goalSchema = mongoose.Schema({
+  text: {
+    type: String,
+    required: [true, 'Please add a text value']
+  }
+}, {
+  timestamps: true
+})
+
+module.exports = mongoose.model('Goal', goalSchema)
